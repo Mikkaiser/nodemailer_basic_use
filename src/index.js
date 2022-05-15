2b0430535a22e8368ced0ee8d687be7297a2153e
mikkaiser <mikaelrsimoes19@gmail.com>
2022-05-15 15:21:39 -0300

    initial commit - set sendmail route

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+
+
+const app = express();
+
+
+app.get('/sendmail', async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+          user: process.env.MAIL_AUTH_USER,
+          pass: process.env.MAIL_AUTH_PASS
+        }
+    })
+
+    const mailsent = await transporter.sendMail({
+        text: 'Texto do e-mail',
+        subject: 'Assunto do e-mail',
+        from: 'Mikael Ribeiro <mikaelrsimoes19@gmail.com>',
+        to: ['mikaelrsimoes19@gmail.com']
+    });
+
+    return res.send(mailsent).json();
+})
+
+app.listen(3000, () => console.log('Server is running'));
